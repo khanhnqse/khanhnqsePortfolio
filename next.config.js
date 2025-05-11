510a68f445bd28d157e46566bd2291a899391aa9
khanhnqse <khanhqn03@gmail.com>
2025-05-11 13:41:25 +0700

    feat:

--- next.config.js
+++ next.config.js
@@ -11,6 +11,22 @@ const nextConfig = {
       ],
     },
   },
+  // Special handling for images with uppercase extensions
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(png|jpe?g|gif|svg|webp|JPG)$/i,
+      use: [
+        {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'static/images/',
+          },
+        },
+      ],
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
